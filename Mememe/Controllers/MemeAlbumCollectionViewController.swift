78dd87311d75d591b6cb947b5dd7180d44dc47f3
yutae <93yutaechoi@gmail.com>
2017-01-23 23:04:41 +0900

    Connect Detail View, Remove Realm DB

--- Mememe/Controllers/MemeAlbumCollectionViewController.swift
+++ Mememe/Controllers/MemeAlbumCollectionViewController.swift
@@ -19,10 +19,13 @@ class MemeAlbumCollectionViewController: UICollectionViewController {
         super.viewDidLoad()
         
         
-        memes = memeDataManager.fetchMemesForAlbum()
         self.setupLayout()
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        memes = memeDataManager.fetchMemesForAlbum()
+        self.collectionView?.reloadData()
+    }
     func setupLayout(){
         let space: CGFloat
         let width: CGFloat
@@ -60,34 +63,17 @@ class MemeAlbumCollectionViewController: UICollectionViewController {
     }
 
     // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: AppModel.memeShowDetailSegueIdentifier, sender: self.memes[indexPath.item])
     }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == AppModel.memeShowDetailSegueIdentifier {
+            let destination = segue.destination as! MemeDetailViewController
+            if let meme = sender as? Meme {
+                destination.selectedMeme = meme
+            }
+        }
     }
-    */
 
 }
--- Mememe/Controllers/MemeDetailViewController.swift
+++ Mememe/Controllers/MemeDetailViewController.swift
@@ -10,13 +10,15 @@ import UIKit
 
 class MemeDetailViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
-    var selectedImage:UIImage?
+    var selectedMeme:Meme?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Optional Binding
-        if let image = self.selectedImage {
-            self.imageView.image = image
+        if let meme = self.selectedMeme {
+            self.imageView.image = meme.image
         }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,8 +27,8 @@ class MemeDetailViewController: UIViewController {
     }
     @IBAction func shareAction(_ sender: Any) {
         // Optional Binding
-        if let image = self.selectedImage {
-            let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        if let meme = self.selectedMeme {
+            let activityViewController = UIActivityViewController(activityItems: [meme.image], applicationActivities: nil)
             // Air Drop 잘 안쓰기 때문에 생략.
             activityViewController.excludedActivityTypes = [UIActivityType.airDrop]
             // Completion Handler
